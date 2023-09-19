1c6424d9e5ce4677c1f0ee48978fe6c89aab7587
Michael Hofmair <michael@hofmair.net>
2023-09-19 10:41:07 +0000

    :rewind: index.php

---
+++ php/public/index.php
@@ -0,0 +1,15 @@
+<?php
+$url = ltrim($_SERVER['REQUEST_URI'], '/');
+
+if ($url === 'api') {
+    $page = file_get_contents('./apiQuestion.php', true);
+}
+if ($url==='api/fetch'){
+    require_once ("get_questions.php");
+    return;
+}
+if(!isset($page)){
+    $page=file_get_contents('././qgame.php', true);
+}
+echo $page;
+?>
